1d88add75e6fc56f36ccf84d0d2660fe3314fdd7
Kirill Peshin <Kirill_Peshin@epam.com>
2021-01-11 13:44:30 +0300

    Initial commit

---
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -0,0 +1,24 @@
+package com.epam;
+
+public class CalcServiceImpl implements CalcService {
+
+    @Override
+    public int add(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        return 0;
+    }
+}
