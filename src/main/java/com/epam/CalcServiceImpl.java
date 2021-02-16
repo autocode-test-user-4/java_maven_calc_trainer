ddd3969804d8ec872e5f6e57445327da256ee28f
autocode-test-contributor-1 <70589511+autocode-test-contributor-1@users.noreply.github.com>
2021-02-16 11:52:23 +0300

    Update CalcServiceImpl.java

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -1,24 +1,26 @@
 package com.epam;
 
 public class CalcServiceImpl implements CalcService {
-
     @Override
     public int add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     @Override
     public int multiply(int a, int b) {
-        return 0;
+        return a * b;
     }
 
     @Override
     public int subtract(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     @Override
     public int divide(int a, int b) {
-        return 0;
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
     }
 }
