3d7a0f03f90b618cc61492a970c79ac0178e3cad
Kirill Peshin <kirill_peshin@epam.com>
2021-06-09 16:01:43 +0300

    fail_test

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -3,24 +3,21 @@ package com.epam;
 public class CalcServiceImpl implements CalcService {
     @Override
     public int add(int a, int b) {
-        return a + b;
+        return a;
     }
 
     @Override
     public int multiply(int a, int b) {
-        return a * b;
+        return a;
     }
 
     @Override
     public int subtract(int a, int b) {
-        return a - b;
+        return a;
     }
 
     @Override
     public int divide(int a, int b) {
-        if (b == 0) {
-            return 0;
-        }
-        return a / b;
+        return a;
     }
 }
